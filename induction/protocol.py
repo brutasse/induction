89942d7e137d5d8e773dda027c36cdacf53525a2
Bruno Renié <brutasse@gmail.com>
2014-09-17 14:48:17 +0200

    Fix response drain

--- induction/protocol.py
+++ induction/protocol.py
@@ -20,4 +20,4 @@ class AppServerHttpProtocol(ServerHttpProtocol):
         return (yield from self.app.handle_request(request, response, payload))
 
     def prepare_response(self, request):
-        return Response(self.transport, 200, request=request)
+        return Response(self.writer, 200, request=request)
